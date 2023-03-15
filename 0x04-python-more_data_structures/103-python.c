2ed3333a5342b530888b9276e09374bf56334b10
Real939 <danimulugeta71@gmail.com>
2023-03-15 10:12:44 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <python.h>
+#include <Python.h>
 
 /**
  * print_python_bytes - Prints bytes information
@@ -66,5 +66,5 @@ void print_python_list(PyObject *p)
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
-}
+	}
 
